a64a4a34962d7644ee77d7611bf5148c55a8c8b3
18badr19-sudo <18badr19@gmail.com>
2026-09-21 00:03:57 +0300

    Update market_regime.py

--- market_regime.py
+++ market_regime.py
@@ -1,6 +1,8 @@
 class MarketRegimeDetector:
-    def __init__(self):
-        pass
+    def __init__(self, high_vix_threshold: float = 25.0, price_change_threshold: float = 0.75):
+        # تفعيل المعلمات وتخزين العتبات للتحكم بمرونة النظام
+        self.high_vix_threshold = high_vix_threshold
+        self.price_change_threshold = price_change_threshold
 
     def detect_regime(self, spy_price_change: float, vix_level: float) -> dict:
         """
@@ -10,13 +12,13 @@ class MarketRegimeDetector:
         regime = "SIDEWAYS"
         risk_profile = "NEUTRAL"
 
-        if vix_level > 25.0:
+        if vix_level > self.high_vix_threshold:
             regime = "HIGH_VOLATILITY_PANIC"
             risk_profile = "DEFENSIVE"
-        elif spy_price_change > 0.75:
+        elif spy_price_change > self.price_change_threshold:
             regime = "BULL_TREND"
             risk_profile = "AGGRESSIVE_GROWTH"
-        elif spy_price_change < -0.75:
+        elif spy_price_change < -self.price_change_threshold:
             regime = "BEAR_TREND"
             risk_profile = "CAPITAL_PRESERVATION"
 
